0830e5ecb256d411350c116f27652b048c5cc384
xechoz <zheng1733@gmail.com>
2016-11-27 22:02:03 +0800

    拼写错误

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 MY_DIR=`pwd`
 echo $MY_DIR
-forever start -a -l $MY_DIR/log/forever.log -o $MY_DIR/log/out.log -e l$MY_DIR/log/err.log $MY_DIR/bin/www
+forever start -a -l $MY_DIR/log/forever.log -o $MY_DIR/log/out.log -e $MY_DIR/log/err.log $MY_DIR/bin/www
