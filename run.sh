86364e4fb198a38cd44bf2a45cbfc99e3c91ecd5
xechoz <zheng1733@gmail.com>
2016-12-13 00:00:42 +0800

    switch to forever

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # NODE_ENV='development' 
-NODE_ENV='production' APP_VERSION='1.0.0' pm2 start $(pwd)/bin/www
+NODE_ENV='production' APP_VERSION='1.0.0' forever start $(pwd)/bin/www
 
 
